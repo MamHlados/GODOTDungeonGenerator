aaae5ceb40bc21e779039dc9166fe66ba9683d54
MamHlados <robinhavlina9@gmail.com>
2026-03-30 20:18:19 +0200

    new room

--- Dungoen Generator/Player.gd
+++ Dungoen Generator/Player.gd
@@ -7,7 +7,7 @@ extends CharacterBody2D
 var character_direction : Vector2
 
 var normal_zoom = Vector2(4, 4)
-var whole_map_zoom = Vector2(0.2, 0.2)
+var whole_map_zoom = Vector2(0.15, 0.15)
 var map_view = false
 
 var last_facing_direction = "down"
